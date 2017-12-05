9dbcc6f29fab79b17a2658c683d79c5646d3de9b
Simon <simon@brulhart.me>
2017-12-05 18:17:23 +0100

    Use stdlib to retrieve terminal size

--- pick/screen_win.py
+++ pick/screen_win.py
@@ -1,30 +1,7 @@
-import ctypes
-import ctypes.wintypes
 import msvcrt
+import shutil
 import sys
 from itertools import islice
-
-
-STD_OUTPUT_HANDLE = -11
-
-class COORD(ctypes.Structure):
-    _fields_ = [("X", ctypes.c_short),
-                ("Y", ctypes.c_short)]
-
-
-class SMALL_RECT(ctypes.Structure):
-    _fields_ = [("Left", ctypes.c_short),
-                ("Top", ctypes.c_short),
-                ("Right", ctypes.c_short),
-                ("Bottom", ctypes.c_short)]
-
-
-class CONSOLE_SCREEN_BUFFER_INFO(ctypes.Structure):
-    _fields_ = [("dwSize", COORD),
-                ("dwCursorPosition", COORD),
-                ("wAttributes", ctypes.c_short),
-                ("srWindow", SMALL_RECT),
-                ("dwMaximumWindowSize", COORD)]
 
 
 class Screen:
@@ -60,21 +37,10 @@ class Screen:
         else:
             return ord(char)
 
-    @staticmethod
-    def _screen_info():
-        hout = ctypes.windll.kernel32.GetStdHandle(STD_OUTPUT_HANDLE)
-        info = CONSOLE_SCREEN_BUFFER_INFO()
-        ctypes.windll.kernel32.GetConsoleScreenBufferInfo(
-            hout, ctypes.byref(info)
-        )
-        return info
-
     @property
     def rows(self):
-        info = self._screen_info()
-        return info.srWindow.Bottom - info.srWindow.Top + 1
+        return shutil.get_terminal_size().lines
 
     @property
     def columns(self):
-        info = self._screen_info()
-        return info.srWindow.Right - info.srWindow.Left
+        return shutil.get_terminal_size().columns - 1
